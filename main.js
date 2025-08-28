932572fa00b4100a1336e7ef502f3b6ed3949894
Rémi Lebret <7931261+lebretr@users.noreply.github.com>
2025-08-28 23:17:42 +0200

    autoHideMenuBar

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 const { app, BaseWindow, WebContentsView } = require('electron');
 
 const createWindow = () => {
-    const win = new BaseWindow({ width: 800, height: 400 /*, fullscreen :true*/});
+    const win = new BaseWindow({ width: 800, height: 400, autoHideMenuBar: true /*, fullscreen :true*/});
     win.maximize();
 
     const winBounds=win.getBounds();
 
     const view1 = new WebContentsView();
+    const widthOffset=5;
+    const heightOffset=5;
     win.contentView.addChildView(view1);
     view1.webContents.loadURL('http://google.com');
-    view1.setBounds({ x: 0, y: 0, width: winBounds.width-20, height: winBounds.height-20 });
+    view1.setBounds({ x: 0, y: 0, width: winBounds.width-widthOffset, height: winBounds.height-heightOffset });
 
     win.on('maximize',() => {
         const winBounds=win.getBounds();
-        view1.setBounds({ x: 0, y: 0, width: winBounds.width-20, height: winBounds.height-20 });
+        view1.setBounds({ x: 0, y: 0, width: winBounds.width-widthOffset, height: winBounds.height-heightOffset });
     });
     win.on('unmaximize',() => {
         const winBounds=win.getBounds();
-        view1.setBounds({ x: 0, y: 0, width: winBounds.width-20, height: winBounds.height-20 });
+        view1.setBounds({ x: 0, y: 0, width: winBounds.width-widthOffset, height: winBounds.height-heightOffset });
     });
     win.on('restore',() => {
         const winBounds=win.getBounds();
-        view1.setBounds({ x: 0, y: 0, width: winBounds.width-20, height: winBounds.height-20 });
+        view1.setBounds({ x: 0, y: 0, width: winBounds.width-widthOffset, height: winBounds.height-heightOffset });
     });
     win.on('resize',() => {
         const winBounds=win.getBounds();
-        view1.setBounds({ x: 0, y: 0, width: winBounds.width-20, height: winBounds.height-20 });
+        view1.setBounds({ x: 0, y: 0, width: winBounds.width-widthOffset, height: winBounds.height-heightOffset });
     });
     win.on('enter-full-screen',() => {
         const winBounds=win.getBounds();
-        view1.setBounds({ x: 0, y: 0, width: winBounds.width-20, height: winBounds.height-20 });
+        view1.setBounds({ x: 0, y: 0, width: winBounds.width-widthOffset, height: winBounds.height-heightOffset });
     });
     win.on('leave-full-screenn',() => {
         const winBounds=win.getBounds();
-        view1.setBounds({ x: 0, y: 0, width: winBounds.width-20, height: winBounds.height-20 });
+        view1.setBounds({ x: 0, y: 0, width: winBounds.width-widthOffset, height: winBounds.height-heightOffset });
     });
 }
 
